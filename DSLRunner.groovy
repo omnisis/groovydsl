b8f6dff683853d325716136f1fa4809e7e3ee842
Clifford James <clifford.h.james@gmail.com>
2012-01-23 02:06:57 -0500

    updates for blog post

--- DSLRunner.groovy
+++ DSLRunner.groovy
@@ -1,16 +1,97 @@
 class DSLRunner {
 
-    static void loadDSL(script) {
+  def charTypes = []
+  
+  /*
+   * This class will hold details about each charactertype
+   */
+  static class CharacterType { 
+    def _name
+    def _description
+    def _disposition
+    def _bonuses = [:]
+    def _baseAttrs = [:]
+    
+    CharacterType(name) { 
+      _name = name
     }
     
-    static void usage() {
-        println "usage: DSLRunner <scriptFile>\n"
-        System.exit(1)
+    def disposition(disp) { 
+      _disposition = disp
     }
+    def description(desc) { 
+      _description = desc
+    }
+    def baseAttrs(Map m) { 
+      _baseAttrs = m
+    }
+    def bonuses(Map m) { 
+      _bonuses = m
+    }
+    
+    String toString() { 
+      "\n${_name}:${_disposition} (${_description})\n\t${_baseAttrs}\n\t${_bonuses}"
+    }
+    
+  };
 
-    static void main(String [] args) {
-        if(args.length < 1) { usage() }
-        GroovyShell shell = new GroovyShell()
-        shell.evaluate(new File(args[0]))
+
+
+  static enum DISPTYPE { 
+    GOOD,
+    EVIL,
+    NEUTRAL
+  }
+
+  void loadDSL(Closure cl) {
+    println "loading DSL ..."
+    cl.delegate = this
+    cl()
+  }
+
+  // intercept missing methods, if they look like
+  //     methodName { ... }
+  // treat them like a new character type definition
+  def methodMissing(String name, args) {
+    println "methodMissing: ${name}"
+    if(args.length == 1 && args[0] instanceof Closure) { 
+      println "encountered new character archetype: ${name}"    
+      newCharacterType(name, args[0])
     }
+  }
+
+  def newCharacterType(name, cl) { 
+    CharacterType cType = new CharacterType(name)
+    cl.delegate = cType
+    cl()
+    this.charTypes += cType
+  }
+  
+  void usage() {
+    println "usage: DSLRunner <scriptFile>\n"
+    System.exit(1)
+  }
+  
+  static void main(String [] args) {
+    DSLRunner runner = new DSLRunner()
+    if(args.length < 1) { runner.usage() }
+    
+    def script = new File(args[0]).text
+    def dsl = """
+	  run {
+	    ${script}
+	  }
+	"""
+    
+    def binding = new Binding()
+    binding.run = { Closure cl -> runner.loadDSL(cl) }	 
+    binding.GOOD = DISPTYPE.GOOD
+    binding.EVIL = DISPTYPE.EVIL
+    binding.NEUTRAL = DISPTYPE.NEUTRAL
+    GroovyShell shell = new GroovyShell(binding)
+    shell.evaluate(dsl)
+
+    // print out character types
+    runner.charTypes.each {  println it }
+  }
 }
--- rpg.dsl
+++ rpg.dsl
@@ -1,26 +1,20 @@
-races {
-    wood_elf {
-        disposition GOOD
-        description "Sharp-witted, pointy-eared lovers of nature, art and song" 
-        baseAttrs [STR: 25,  AGILITY: 45, DEX: 15, WILLPWR: 25, INT: 60, CHA: 35]
-        bonuses [MAGICKA: +10, MUSIC: +5, ARCHERY: +20]
-        specialAbilities ['TREE_WHISPERER']
-    }
-    human {
-        disposition NEUTRAL
-        description "Diverse race with broad-ranging abilities and strong willpower"
-        baseAttrs [STR: 25,  AGILITY: 30, DEX: 25, WILLPWR: 50, INT: 40, CHA: 45]
-        bonuses [ARMOR: +10]
-        specialAbilities ['DUEL_WIELD']
-
-    }
-    troll {
-        disposition EVIL
-        description "Despicable creatures that care only about their hunger for food and lust for battle."
-        baseAttrs [STR: 40, AGILITY: 25, DEX: 10, WILLPWR: 25, INT: 10, CHA: 0]
-        bonuses ['BERSERKERdoc ']
-        specialAbilities ['EAT_CHILDREN']
-
-    }
-
+wood_elf {
+      disposition GOOD
+      description "Sharp-witted, pointy-eared lovers of nature, art and song" 
+      baseAttrs(STR: 25,  AGILITY: 45, DEX: 15, WILLPWR: 25, INT: 60, CHA: 35)
+      bonuses(MAGICKA: +10, MUSIC: +5, ARCHERY: +20)
 }
+human {
+       disposition NEUTRAL
+       description "Diverse race with broad-ranging abilities and strong willpower"
+       baseAttrs(STR: 25,  AGILITY: 30, DEX: 25, WILLPWR: 50, INT: 40, CHA: 45)
+       bonuses(ARMOR: +10)
+}
+troll {
+       disposition EVIL
+       description "Despicable creatures that care only about their hunger for food and lust for battle."
+       baseAttrs(STR: 40, AGILITY: 25, DEX: 10, WILLPWR: 25, INT: 10, CHA: 0)
+       bonuses(RESIST_FIRE: +10)
+}
+
+
